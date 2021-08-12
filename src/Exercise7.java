8f405f21dc2883d6347f2676cbbb96f43bd3ab80
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-08-12 13:23:26 +1000

    Add java class and txt file for exercise7

---
+++ src/Exercise7.java
@@ -0,0 +1,16 @@
+import org.w3c.dom.Text;
+import textio.TextIO;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        TextIO.readFile("testdata.txt");
+        String name = TextIO.getln();
+        int resultOne = TextIO.getlnInt();
+        int resultTwo = TextIO.getlnInt();
+        int resultThree = TextIO.getlnInt();
+        double averageResult;
+        averageResult = (resultOne + resultTwo + resultThree) / 3.0;
+        System.out.printf("%s scored %d, %d, %d for an average of: %1.1f", name, resultOne, resultTwo, resultThree, averageResult);
+        System.out.println();
+    }
+}
